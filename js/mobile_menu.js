0a6a83d20069d25d282e0e665a3687670bec5157
Nelson Gabriel Hernández <gabrielymarg@gmail.com>
2022-04-15 06:05:28 -0300

    Commit > Header terminado... Comienzo de footer

--- js/mobile_menu.js
+++ js/mobile_menu.js
@@ -1,19 +1,28 @@
 function mobileMenuToggler() {
     if ( menu.style.display === "block" ) {
         menu.style.display = "none";
-        menu_icon.innerHTML = barsIcon;
+        barsIcon.style.display = "block";
+        closeIcon.style.display = "none";
+        infoBox.style.display = "block";
     } else {
         menu.style.display = "block";
-        menu_icon.innerHTML = closeIcon;
+        barsIcon.style.display = "none";
+        closeIcon.style.display = "block";
+        infoBox.style.display = "none";
     }
 }
 
 const menu_icon = document.getElementById('nav__icon');
 const menu = document.getElementById('nav__main');
-const barsIcon = '<i class="fa-solid fa-bars"></i>';
-const closeIcon = '<i class="fa-solid fa-xmark"></i>';
+const barsIcon = document.getElementById('nav__icon-bars');
+const closeIcon = document.getElementById('nav__icon-xmark');
+const infoBox = document.getElementById('header__info-box');
 
 if (document.body.clientWidth < 992) {
-    menu_icon.innerHTML = barsIcon;
     menu.style.display = "none";
+    barsIcon.style.display = "block";
+    closeIcon.style.display = "none";
+} else {
+    barsIcon.style.display = "none";
+    closeIcon.style.display = "none";
 }
