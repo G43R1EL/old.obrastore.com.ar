a5ae8a605af27ad161f3b4b9d015bba29a8ec3af
Nelson Gabriel Hernández <gabrielymarg@gmail.com>
2022-04-15 16:09:27 -0300

    Commit > Mejoras en header... Footer terminado

--- js/mobile_menu.js
+++ js/mobile_menu.js
@@ -1,11 +1,11 @@
 function mobileMenuToggler() {
-    if ( menu.style.display === "block" ) {
+    if ( menu.style.display === "flex" ) {
         menu.style.display = "none";
         barsIcon.style.display = "block";
         closeIcon.style.display = "none";
         infoBox.style.display = "block";
     } else {
-        menu.style.display = "block";
+        menu.style.display = "flex";
         barsIcon.style.display = "none";
         closeIcon.style.display = "block";
         infoBox.style.display = "none";
@@ -17,12 +17,3 @@ const menu = document.getElementById('nav__main');
 const barsIcon = document.getElementById('nav__icon-bars');
 const closeIcon = document.getElementById('nav__icon-xmark');
 const infoBox = document.getElementById('header__info-box');
-
-if (document.body.clientWidth < 992) {
-    menu.style.display = "none";
-    barsIcon.style.display = "block";
-    closeIcon.style.display = "none";
-} else {
-    barsIcon.style.display = "none";
-    closeIcon.style.display = "none";
-}
