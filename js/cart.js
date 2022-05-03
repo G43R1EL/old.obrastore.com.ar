c48353c996bfd133c831789721d8718de842fdeb
Gabriel Hernández <gabrielymarg@gmail.com>
2022-05-03 16:06:53 -0300

    Carrito hecho de cero para organizar mejor...

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,4 @@
+// Arreglo de productos
 const productsList = [
     {
         id: '1000', 
@@ -70,122 +71,133 @@ const productsList = [
     }
 ];
 
-// Agrega items en carrito...
-function agregarItem(idProducto) {
-    let producto = productsList.find(prod => prod.id == idProducto);
-    let cantidad = parseInt(document.getElementById(idProducto + "_qty").value);
-    if (cantidad < 0) {
-        eliminarItem(idProducto,cantidad);
-        return;
+// Genera las tarjetas de productos
+function generateProductsHtml(products) {
+    let html = '';
+    products.forEach(product => {
+        html += `<div class="product__card">
+                     <img src="${product.imagen}"/>
+                     <h3>${product.nombre}</h3>
+                     <p>${product.descripcion}</p>
+                     <p class="price">$ ${product.precio}-</p>
+                     <form id="${product.id}">
+                         <label for="${product.id}_qty">Cantidad</label>
+                         <input type="number" id="${product.id}_qty" name="${product.id}_qty" value="1" min="-99" max="99" required></input>
+                         <button type="button">Agregar</button>
+                     </form>
+                 </div>`;
+    });
+    return html;
+}
+
+// Genera el html del carrito
+function generateCartHtml() {
+    let html = `<table id="tbl__cart">
+                    <thead>
+                        <tr>
+                            <th>Producto</th>
+                            <th>Cantidad</th>
+                            <th>Precio</th>
+                            <th>Total</th>
+                            <th>Acciones</th>
+                            </tr>
+                            </thead>
+                            <tbody>`;
+    cart.forEach(product => {
+        html += `<tr>
+                    <td>${product.nombre}</td>
+                    <td>${product.cantidad}</td>
+                    <td>$ ${product.precio}</td>
+                    <td>$ ${product.precio * product.cantidad}</td>
+                    <td><i class="fa-solid fa-trash-can" id="removeBtn_${product.id}"></i></td>
+                </tr>`;
+    });
+    return html;
+}
+
+// Agrega event listener a los botones de agregar producto
+function addEventListenersToForms() {
+    const forms = document.querySelectorAll('form');
+    forms.forEach(form => {
+        form.querySelector('button').addEventListener('click', () => {
+            const id = form.id;
+            const qty = form.querySelector('input').valueAsNumber;
+            addProductToCart(id, qty);
+        });
+    });
+}
+
+// Agrega event listener a los botones de eliminar producto
+function addEventListenersToDeleteButtons() {
+    const deleteButtons = document.querySelectorAll('.fa-solid.fa-trash-can');
+    deleteButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const id = button.id.split('_')[1];
+            const qty = (cart.find(product => product.id === id).cantidad) * -1;
+            removeProductFromCart(id, qty);
+            refreshCart();
+        });
+    });
+}
+
+// Obtiene el producto por id
+function getProductById(id) {
+    return productsList.find(product => product.id === id);
+}
+
+// Agrega productos al carrito
+function addProductToCart(productId, qty) {
+    const product = getProductById(productId);
+    const cartItem = {
+        id: productId,
+        nombre: product.nombre,
+        descripcion: product.descripcion,
+        precio: product.precio,
+        cantidad: qty
     };
-    if (cantidad >= 1) {
-        if (!existe(idProducto)) {
-            producto.cantidad = cantidad;
-            producto.subtotal = producto.cantidad * producto.precio;
-            carrito.push(producto);
-            showPopup('Se agrego el producto...');
+    if (cartItem.qty < 0) { // Si cantidad es negativa quita producto del carrito
+        removeProductFromCart(productId, qty);
+    } else if (qty > 0) { // Si cantidad es positiva agrega producto al carrito
+        // Verifica si el producto ya esta en el carrito
+        const idx = cart.findIndex(item => item.id === productId);
+        if (idx === -1) {
+            cart.push(cartItem);
+            showPopup(`${qty} ${cartItem.nombre} agregado al carrito`);
         } else {
-            let idx = carrito.findIndex(prod => prod.id == idProducto);
-            carrito[idx].cantidad += cantidad;
-            carrito[idx].subtotal = carrito[idx].cantidad * carrito[idx].precio;
-            showPopup(`Se agrego el producto...`);
+            cart[idx].cantidad += qty;
+            showPopup(`${qty} ${cartItem.nombre} agregado al carrito`);
         }
-    } else {
-        showPopup('No se reconoce valor ingresado...');
+    } else { // Si cantidad es 0 advertencia
+        showPopup('Cantidad no válida');
     }
 }
 
-// Quita items del carrito
-function eliminarItem(idProducto, cantidad) {
-    cantidad *= -1;
-    if (existe(idProducto) && cantidad > 0) {
-        let idx = carrito.findIndex( prod => prod.id == idProducto );
-        if (carrito[idx].cantidad > cantidad) {
-            carrito[idx].cantidad -= cantidad;
-            carrito[idx].subtotal = carrito[idx].cantidad * carrito[idx].precio;
-            showPopup(`Se quito...`);
-        } else {
-            carrito.splice(idx,1);
-            showPopup('Producto eliminado...');
+// Quita productos del carrito
+function removeProductFromCart(productId, qty) {
+    const idx = cart.findIndex(item => item.id === productId);
+    if (idx !== -1) {
+        cart[idx].cantidad += qty;
+        showPopup(`${qty} ${cart[idx].nombre} del carrito`);
+        if (cart[idx].cantidad <= 0) { // Si ya no hay existencias en el carrito quita el producto
+            showPopup(`${cart[idx].nombre} eliminado del carrito`);
+            cart.splice(idx, 1);
         }
-    } else {
-        showPopup('No se reconoce valor ingresado...');
     }
 }
 
-// Toma la variable productos y genera con ella el html necesario para armar tarjetas de producto.
-function listarProductos() {
-    let htmlProductsList = '';
-    for (const product of productsList) {
-        htmlProductsList +=    `<div class="product__card">
-                                    <img src="${product.imagen}"/>
-                                    <h3>${product.nombre}</h3>
-                                    <p>${product.descripcion}</p>
-                                    <p class="price">$ ${product.precio}-</p>
-                                    <form id="${product.id}">
-                                        <label for="${product.id}_qty">Cantidad</label>
-                                        <input type="number" id="${product.id}_qty" name="${product.id}_qty" value="1" min="-99" max="99" required></input>
-                                        <button type="button" onClick="agregarItem(this.parentNode.id)">Agregar</button>
-                                    </form>
-                                </div>`;
-    };
-    return htmlProductsList;
-}
-
-// Verifica si un elemento ya existe en el carrito.
-function existe(idProducto) {
-    if (carrito.find(prod => prod.id == idProducto) === undefined) {
-        return false;
-    } else {
-        return true;
+// Guarda el carrito en Local Storage
+function saveCart() {
+    if (cart.length > 0) {
+        localStorage.setItem('cart', JSON.stringify(cart));
     }
 }
 
-// Muestra lista de productos y total de la cuenta
-function muestraCarrito () {
-    addModal();
-    tablaCarrito();
-}
-
-// Genera contenido de la tabla del carrito
-function tablaCarrito () {
-    let modalContent = document.querySelector('#modal__content');
-    let message = '';
-    let tabla = `<table id="tbl__carrito">
-                 <tr><th>Producto</th><th>Precio</th><th>Cantidad</th><th>Subtotal</th><th>Acciones</th></tr>\n`;
-    let total = 0;
-    for (producto of carrito) {
-        tabla = tabla + `<tr><td>${producto.nombre}</td><td>${producto.precio}</td><td>${producto.cantidad}</td><td>${producto.subtotal}</td><td><i class="fa-solid fa-trash-can" id="${producto.id}" onClick="eliminarItem(this.id,${producto.cantidad}*-1);tablaCarrito();showPopup('Producto eliminado...');"></i></td></tr>`;
-        total += Number(producto.subtotal);
+// Carga el carrito de Local Storage
+function loadCart() {
+    const cartData = localStorage.getItem('cart');
+    if (cartData) {
+        cart = JSON.parse(cartData);
     }
-    tabla = tabla + `</table>
-                     <p>Total : $ ${total}</p>
-                     <button id="cart__save" type="button" onClick="guardaCarrito()">Guardar carrito...</button>`;
-    message = tabla;
-    modalContent.innerHTML = message;
-    modalContent.style.display = 'flex';
-    modalContent.style.flexDirection = 'column';
-}
-
-// Guarda el carrito como JSON en LocalStorage
-function guardaCarrito () {
-        if (carrito.length) {
-            let carritoJSON = JSON.stringify(carrito);
-            localStorage.setItem('carrito', carritoJSON);
-            showPopup('Carrito guardado...');
-        } else {
-            localStorage.removeItem('carrito');
-            showPopup('Carrito limpio...');
-        }
-}
-
-// Carga el carrito de LocalStorage
-function cargarCarrito () {
-    let carritoJSON = localStorage.getItem('carrito');
-    if (carritoJSON) {
-        carrito = JSON.parse(carritoJSON);
-        showPopup('Se cargo carrito de sesión anterior...');
-    };
 }
 
 // Muestra mensaje emergente
@@ -195,15 +207,32 @@ function showPopup (message) {
     setTimeout ( function () { popup.style.display='none' }, 1000 );
 }
 
-// Carrito de compras
-let carrito = [];
+// Muestra el carrito
+function showCart() {
+    addModal();
+    refreshCart();
+}
 
+// Refresca el carrito
+function refreshCart() {
+    const cartHtml = generateCartHtml();
+    const cartContainer = document.querySelector('#modal__content');
+    cartContainer.innerHTML = cartHtml;
+    if (cart.length > 0) {
+        addEventListenersToDeleteButtons();
+    }
+}
+
+// Crea el carrito
+let cart = [];
+// Carga el carrito de Local Storage
+loadCart();
+// Carga el html de productos en el div#products
+document.getElementById('products').innerHTML = generateProductsHtml(productsList);
 // Prepara los mensajes emergentes...
 document.querySelector('body').insertAdjacentHTML("afterbegin","<div id='popup__message'></div>");
 const popup = document.querySelector('#popup__message');
-// Obtiene el contenedor de los productos y luego asigna el html generado por la función listarProductos().
-const mainContent = document.querySelector('#products');
-const cartButton = document.querySelector('.fa-cart-shopping');
-mainContent.innerHTML = listarProductos();
-cartButton.addEventListener("click", muestraCarrito);
-cargarCarrito();
+// Agrega event listeners a los botones de agregar producto
+addEventListenersToForms();
+// Agrega event listener al boton de carrito
+document.getElementsByClassName('fa-cart-shopping')[0].addEventListener('click', showCart);
